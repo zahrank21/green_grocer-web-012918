2e52708671cf00319c1f69039d7c4bcfa17ee7a8
zahrank21 <zahrank@gmail.com>
2018-01-20 12:30:52 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,70 @@
 def consolidate_cart(cart)
-  # code here
+
+
+  result  = {}
+  count_arr = []
+  cart.each do |item_hash|
+    item_hash.each do |item, att|
+      count_arr << item
+    end
+  end
+
+
+  final_hash = {}
+
+  cart.each do |item_hash|
+    item_hash.each do |k,v|
+    final_hash[k] = v
+    end
+  end
+    count_arr.each do |el|
+      final_hash[el][:count] = 0
+    end
+    count_arr.each do |el|
+      final_hash[el][:count] += 1
+    end
+
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  final = {}
+  cart.each do |food, info|
+      coupons.each do |coupon|
+        if food == coupon[:item] && info[:count] >= coupon[:num]
+          info[:count] =  info[:count] - coupon[:num]
+          if final["#{food} W/COUPON"]
+            final["#{food} W/COUPON"][:count] += 1
+          else
+            final["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
+          end
+        end
+      end
+      final[food] = info
+    end
+    final
 end
 
 def apply_clearance(cart)
-  # code here
+  final = {}
+ cart.each do |food, info|
+    final[food] = {}
+    if info[:clearance] == true
+      final[food][:price] = info[:price] * 4 / 5
+    else
+      final[food][:price] = info[:price]
+    end
+    final[food][:clearance] = info[:clearance]
+    final[food][:count] = info[:count]
+  end
+  final
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidated = 
+  couponed_cart = apply_coupons(cart, coupons)
+  pre_discount = apply_clearance(couponed_cart)
+
+  print pre_discount
+  pre_discount
 end
